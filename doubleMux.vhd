26fa601112d3613dcdc9b36a60c94b7447ac23f3
OliverJunk <junk.oliver@gmx.de>
2016-03-11 13:52:41 +0100

    new stuff

--- doubleMux.vhd
+++ doubleMux.vhd
@@ -106,64 +106,144 @@ end Func;
 
 --*=======================*=====================
 
---	Now we write the definition SR-Latch
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+    use ieee.std_logic_1164.all;
 
-entity S_R_latch_top is
-    Port ( A : in    STD_LOGIC;
-           B : in    STD_LOGIC;
-           F : inout STD_LOGIC); -- changed out to inout
-end S_R_latch_top;
+entity dff_sync_reset is
+    port (
+        data  :in  std_logic; -- Data input
+        clk   :in  std_logic; -- Clock input
+        q     :out std_logic  -- Q output
 
-architecture Behavioral of S_R_latch_top is
-signal notQ : STD_LOGIC;
+    );
+end entity;
+
+architecture rtl of dff_sync_reset is
+
 begin
+    process (clk) begin
+        if (rising_edge(clk)) then
+                q <= data;
+            end if;
+    end process;
 
-F    <= B nor notQ;
-notQ <= A nor F;
-
-end Behavioral;
-
-
+end architecture;
 
 --*=======================*=====================
-
---	Now we write the definition for a Multistage Arbiter
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity MultiArbiter is
-   port( DIP1, DIP2, DIP3, DIP4, DIP5, DIP6, DIP7, DIP8 : in std_logic;
-                 LED1 : out std_logic);
-end MultiArbiter;
+entity bitStage is
+   port( A, B, C, D, E, F, G, H, I, J : in std_logic;
+                 P, Q : out std_logic);
+end bitStage;
 --
-architecture Behavior of MultiArbiter is
+architecture Func of  bitStage is
 
    component Arbiterstage is      
       port( A, B, C  : in std_logic;
             F, G : out std_logic);
    end component;
 	
-   component S_R_latch_top is       
-      port( A, B  : in std_logic;
-            F : inout std_logic);
+	signal outUp1, outDo1, outUp2, outDo2,outUp3, outDo3, outUp4, outDo4, outUp5, outDo5, outUp6, outDo6, outUp7, outDo7, outUp8, outDo8 : std_logic;
+
+
+
+begin
+	G7: Arbiterstage   port map(A, B, C, outUp1, outDo1); 
+	G8: Arbiterstage   port map(outUp1, outDo1, D, outUp2, outDo2); 
+	G9: Arbiterstage   port map(outUp2, outDo2, E, outUp3, outDo3); 
+	G10: Arbiterstage   port map(outUp3, outDo3, F, outUp4, outDo4); 
+	G11: Arbiterstage   port map(outUp4, outDo4, G, outUp5, outDo5); 
+	G12: Arbiterstage   port map(outUp5, outDo5, H, outUp6, outDo6);  
+	G13: Arbiterstage   port map(outUp6, outDo6, I, outUp7, outDo7);  
+	G14: Arbiterstage   port map(outUp7, outDo7, J, outUp8, outDo8);  
+	G15: P<=outUp8;
+		Q<=outDo8;
+end Func;
+
+
+--*=======================*=====================
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity sevenbitStage is
+   port( A, B, C, D, E, F, G, H, I : in std_logic;
+                 P, Q : out std_logic);
+end sevenbitStage;
+--
+architecture Func of  sevenbitStage is
+
+   component Arbiterstage is      
+      port( A, B, C  : in std_logic;
+            F, G : out std_logic);
    end component;
 	
-   signal outUp1, outDo1, outUp2, outDo2,outUp3, outDo3, outUp4, outDo4, outUp5, outDo5, outUp6, outDo6, ret : std_logic;
+	signal outUp1, outDo1, outUp2, outDo2,outUp3, outDo3, outUp4, outDo4, outUp5, outDo5, outUp6, outDo6, outUp7, outDo7 : std_logic;
+
+
+
+begin
+	G7: Arbiterstage   port map(A, B, C, outUp1, outDo1); 
+	G8: Arbiterstage   port map(outUp1, outDo1, D, outUp2, outDo2); 
+	G9: Arbiterstage   port map(outUp2, outDo2, E, outUp3, outDo3); 
+	G10: Arbiterstage   port map(outUp3, outDo3, F, outUp4, outDo4); 
+	G11: Arbiterstage   port map(outUp4, outDo4, G, outUp5, outDo5); 
+	G12: Arbiterstage   port map(outUp5, outDo5, H, outUp6, outDo6);  
+	G13: Arbiterstage   port map(outUp6, outDo6, I, outUp7, outDo7);   
+	G15: P<=outUp7;
+		Q<=outDo7;
+end Func;
+
+
+
+
+--	Now we write the definition for a Multistage Arbiter
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity MultiArbiter is
+   port( DIP1, DIP2, DIP3, DIP4, DIP5, DIP6, DIP7, DIP8, SWITCH1, SWITCH2 : in std_logic;
+                 LED1, LED2 : out std_logic);
+end MultiArbiter;
+--
+architecture Behavior of MultiArbiter is
+
+   component  bitStage is      
+      port( A, B, C, D, E, F, G, H, I, J  : in std_logic;
+            P, Q : out std_logic);
+   end component;
+   component  sevenbitStage is      
+      port( A, B, C, D, E, F, G, H, I  : in std_logic;
+            P, Q : out std_logic);
+   end component;
+	component Arbiterstage is      
+      port( A, B, C  : in std_logic;
+            F, G : out std_logic);
+   end component;
+   component dff_sync_reset is
+      port( data, clk  : in std_logic;
+            q : inout std_logic);
+   end component;
+	
+   signal outUp1, outDo1, outUp2, outDo2, outUp3, outDo3, outUp4, outDo4, outUp5, outDo5, outUp6, outDo6, outUp7, outDo7, outUp8, outDo8, outUp9, outDo9, ret : std_logic;
    signal inUp : std_logic;
 	signal inDo : std_logic;
 
 begin
-	inUp<='1';
-	inDo<='1';
-	G7: Arbiterstage   port map(inUp, inDo, DIP3, outUp1, outDo1); 
-	G8: Arbiterstage   port map(outUp1, outDo1, DIP4, outUp2, outDo2); 
-	G9: Arbiterstage   port map(outUp2, outDo2, DIP5, outUp3, outDo3); 
-	G10: Arbiterstage   port map(outUp3, outDo3, DIP6, outUp4, outDo4); 
-	G11: Arbiterstage   port map(outUp4, outDo4, DIP7, outUp5, outDo5); 
-	G12: Arbiterstage   port map(outUp5, outDo5, DIP8, outUp6, outDo6);  
-	G13: S_R_latch_top	port map(outUp6, outDo6, ret);
-	G14: LED1<=ret;
+	inUp<= not SWITCH1;
+	inDo<= not SWITCH1;
+	G16: bitStage   port map(inUp, inDo, DIP1, DIP2, DIP3, DIP4, DIP5, DIP6, DIP7, DIP8, outUp1, outDo1); 
+	G17: bitStage   port map(outUp1, outDo1, DIP1, DIP2, DIP3, DIP4, DIP5, DIP6, DIP7, DIP8, outUp2, outDo2);
+	G18: bitStage   port map(outUp2, outDo2, DIP1, DIP2, DIP3, DIP4, DIP5, DIP6, DIP7, DIP8, outUp3, outDo3);
+	G19: bitStage   port map(outUp3, outDo3, DIP1, DIP2, DIP3, DIP4, DIP5, DIP6, DIP7, DIP8, outUp4, outDo4);
+	G20: bitStage   port map(outUp4, outDo4, DIP1, DIP2, DIP3, DIP4, DIP5, DIP6, DIP7, DIP8, outUp5, outDo5);
+	G21: bitStage   port map(outUp5, outDo5, DIP1, DIP2, DIP3, DIP4, DIP5, DIP6, DIP7, DIP8, outUp6, outDo6);
+	G22: bitStage   port map(outUp6, outDo6, DIP1, DIP2, DIP3, DIP4, DIP5, DIP6, DIP7, DIP8, outUp7, outDo7);
+	G23: sevenbitStage   port map(outUp7, outDo7, DIP1, DIP2, DIP3, DIP4, DIP5, DIP6, DIP7, outUp8, outDo8);
+	G24: Arbiterstage port map(outUp8, outDo8, SWITCH2, outUp9, outDo9);
+	G25: dff_sync_reset	port map(outUp9, outDo9, ret);
+	G26:	LED1<=ret;
+	LED2<= not ret;
 end Behavior;
